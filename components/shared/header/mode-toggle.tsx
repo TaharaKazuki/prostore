67bbc770a38430b3d02d3230464af181508eb71e
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-03-07 08:17:47 +0900

    Refactor theme toggle with improved dropdown menu and dynamic theme selection

--- components/shared/header/mode-toggle.tsx
+++ components/shared/header/mode-toggle.tsx
@@ -7,34 +7,58 @@ import { useTheme } from 'next-themes';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
+  DropdownMenuCheckboxItem,
   DropdownMenuContent,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 
-const themeIcons = {
-  light: <SunIcon />,
-  dark: <MoonIcon />,
-  system: <SunMoon />,
-};
+const ThemeList = [
+  {
+    label: 'System',
+    value: 'system',
+    icon: <SunMoon />,
+  },
+  {
+    label: 'Light',
+    value: 'light',
+    icon: <SunIcon />,
+  },
+  {
+    label: 'Dark',
+    value: 'dark',
+    icon: <MoonIcon />,
+  },
+];
 
 const ModeToggle = () => {
   const { theme, setTheme } = useTheme();
 
+  const checkTheme = () => ThemeList.find((item) => item.value === theme)?.icon;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost">
-          {themeIcons[theme as keyof typeof themeIcons] || themeIcons.system}
+        <Button
+          variant="ghost"
+          className="focus-visible:ring-0 focus-visible:ring-offset-0"
+        >
+          {checkTheme()}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
-          <DropdownMenuRadioItem value="system">System</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="light">Light</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="dark">Dark</DropdownMenuRadioItem>
-        </DropdownMenuRadioGroup>
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {ThemeList.map((item) => (
+          <DropdownMenuCheckboxItem
+            key={item.value}
+            checked={theme === item.value}
+            onClick={() => setTheme(item.value)}
+          >
+            {item.label}
+          </DropdownMenuCheckboxItem>
+        ))}
       </DropdownMenuContent>
     </DropdownMenu>
   );
