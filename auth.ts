b2bcd0e7e175569a5d1df8f01e9b32517fe975c0
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-03-11 16:52:49 +0900

    Implement basic credentials authentication in NextAuth

--- auth.ts
+++ auth.ts
@@ -1,5 +1,7 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { compareSync } from 'bcrypt-ts-edge';
 import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 import { prisma } from '@/db/prisma';
 
@@ -13,7 +15,28 @@ export const config = {
     masAge: 30 * 24 * 60 * 60, // 30days
   },
   adapter: PrismaAdapter(prisma),
-  provider,
+  provider: [
+    CredentialsProvider({
+      credentials: {
+        email: { type: 'email' },
+        password: { type: 'password' },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
+
+        // Find user in database
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+
+        if (user && user.password) {
+          const isMatch = compareSync;
+        }
+      },
+    }),
+  ],
 };
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
