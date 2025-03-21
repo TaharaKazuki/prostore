1866de7d73614f77b98c23b22e928ee1d82bacfe
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-03-21 14:04:24 +0900

    Refactor Card components to use forward refs and enhance SignInPage layout with new CardDescription and CardContent

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,7 +4,13 @@ import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 import { auth } from '@/auth';
-import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { APP_NAME } from '@/lib/constants';
 
 export const metadata: Metadata = {
@@ -33,15 +39,18 @@ const SignInPage = async ({ searchParams }: SignInPageProps) => {
           <Link href="/" className="flex-center">
             <Image
               src="/images/logo.svg"
-              alt={`${APP_NAME} logo`}
               width={100}
               height={100}
+              alt={`${APP_NAME} logo`}
               priority={true}
             />
           </Link>
-
-          <CardTitle>Sign in</CardTitle>
+          <CardTitle className="text-center">Sign In</CardTitle>
+          <CardDescription className="text-center">
+            Sign in to your account
+          </CardDescription>
         </CardHeader>
+        <CardContent className="space-y-4"></CardContent>
       </Card>
     </div>
   );
