2eff117ad7bef6993f547540e28991e30c36f919
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-03-21 09:31:27 +0900

    Add logo image and metadata to SignInPage component

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,15 @@
+import type { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 import { auth } from '@/auth';
 import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+import { APP_NAME } from '@/lib/constants';
+
+export const metadata: Metadata = {
+  title: 'Sign in',
+};
 
 type SignInPageProps = {
   searchParams: Promise<{
@@ -21,7 +29,17 @@ const SignInPage = async ({ searchParams }: SignInPageProps) => {
   return (
     <div className="mx-auto w-full max-w-md">
       <Card>
-        <CardHeader>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              alt={`${APP_NAME} logo`}
+              width={100}
+              height={100}
+              priority={true}
+            />
+          </Link>
+
           <CardTitle>Sign in</CardTitle>
         </CardHeader>
       </Card>
