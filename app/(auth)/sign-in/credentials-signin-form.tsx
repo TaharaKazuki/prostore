0e079be4525c8710db100d9f5d4eafc80e09fd6d
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-03-26 09:32:06 +0900

    Implement credentials sign-in form with email and password inputs, sign-in button, and a link to sign-up page

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -1,7 +1,56 @@
 'use client';
 
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+const signInDefaultValues = {
+  email: '',
+  password: '',
+};
+
 const CredentialsSignInForm = () => {
-  return <div>CredentialsSignInForm</div>;
+  return (
+    <form>
+      <div className="space-y-6">
+        <div>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            required
+            autoComplete="email"
+            defaultValue={signInDefaultValues.email}
+          />
+        </div>
+        <div>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            required
+            autoComplete="password"
+            defaultValue={signInDefaultValues.password}
+          />
+        </div>
+        <div>
+          <Button variant={'default'} className="w-full">
+            Sign In
+          </Button>
+        </div>
+        <div className="text-center text-sm text-muted-foreground">
+          Don&apos;t have an account?{' '}
+          <Link href="/sign-up" target="_self">
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default CredentialsSignInForm;
