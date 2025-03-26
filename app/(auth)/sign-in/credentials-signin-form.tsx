a3cb7eb2122bb74cd2980652e7e071bfb2ea9518
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-03-27 07:50:38 +0900

    Update Next.js to version 15.2.4 and refactor CredentialsSignInForm to include callback URL handling and move SignInButton component inside it for better encapsulation.

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 import { useActionState } from 'react';
 import { useFormStatus } from 'react-dom';
 
@@ -14,24 +15,28 @@ const signInDefaultValues = {
   password: '',
 };
 
-const SignInButton = () => {
-  const { pending } = useFormStatus();
-
-  return (
-    <Button disabled={pending} className="w-full" variant="default">
-      {pending ? 'Signing In...' : 'Sign In'}
-    </Button>
-  );
-};
-
 const CredentialsSignInForm = () => {
   const [data, action] = useActionState(signInWithCredentials, {
     success: false,
     message: '',
   });
 
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl') || '/';
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+      <Button disabled={pending} className="w-full" variant="default">
+        {pending ? 'Signing In...' : 'Sign In'}
+      </Button>
+    );
+  };
+
   return (
     <form action={action}>
+      <input type="hidden" name="callbackUrl" value={callbackUrl} />
       <div className="space-y-6">
         <div>
           <Label htmlFor="email">Email</Label>
