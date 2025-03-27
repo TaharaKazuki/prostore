994366c475a7832197447ef97865b72840069254
Tahara Kazuki <ssp0727.lnc@gmail.com>
2025-03-27 09:37:14 +0900

    Add sign-up form validation schema and implement user registration logic with password hashing in user actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,9 +1,11 @@
 'use server';
 
+import { hashSync } from 'bcrypt-ts-edge';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
 
 import { signIn, signOut } from '@/auth';
-import { signInFormSchema } from '@/lib/validators';
+import { prisma } from '@/db/prisma';
+import { signInFormSchema, signUpFormSchema } from '@/lib/validators';
 
 export async function signInWithCredentials(
   prevState: unknown,
@@ -27,4 +29,34 @@ export async function signInWithCredentials(
 
 export async function signOutUser() {
   await signOut();
+}
+
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      password: formData.get('password'),
+      confirmPassword: formData.get('confirmPassword'),
+    });
+
+    const plainPassword = user.password;
+    user.password = hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn('credentials', {
+      email: user.email,
+      password: plainPassword,
+    });
+  } catch (error) {
+    console.error('ユーザー登録エラー:', error);
+    return { success: false, message: 'ユーザー登録に失敗しました' };
+  }
 }
